8e8923590ddad12ddbbc6126c01530aaa74af677
Loïc Formont <loic.formont@gmail.com>
2020-04-27 22:15:33 +0200

    added create

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-# allvalues
+# all-values
+
+```javascript
+import allValues from 'all-values';
+
+return Promise.resolve({})
+    .then((prev) => allValues({...prev, session: enigmaHelper.createSession() }))
+    .then((prev) => allValues({...prev, applications: prev.session.global.getDocList() }))
+    .then((prev) => allValues({...prev, result: prev.applications.map((doc) => ({ id: doc.qDocId, name: doc.qDocName })) }));
+```
+
+you can create an allValues object with your own promise framework
+
+```javascript
+import q from 'q';
+import allValues from 'all-values';
+
+let allValuesQ = allValues.create({ Promise: q });
+
+return Promise.resolve({})
+    .then((prev) => allValuesQ({...prev, session: enigmaHelper.createSession() }))
+    .then((prev) => allValuesQ({...prev, applications: prev.session.global.getDocList() }))
+    .then((prev) => allValuesQ({...prev, result: prev.applications.map((doc) => ({ id: doc.qDocId, name: doc.qDocName })) }));
+```
--- index.js
+++ index.js
@@ -1,20 +1,21 @@
-const allValues = (object) => {
-    var keys = Object.keys(object);
-    var values = [];
-    var key;
+module.exports = createUtils({ Promise: Promise });
+module.exports.create = createUtils;
 
-    for (key in object) {
-        values.push(object[key]);
-    }
-
-    return Promise.all(values).then(function (results) {
-        return keys.reduce(function (fulfilledObject, key, index) {
-            fulfilledObject[key] = results[index];
-            return fulfilledObject;
-        }, {});
-    });
-};
-
-export default {
-    allValues
+function createUtils(config) {
+    return (object) => {
+        var keys = Object.keys(object);
+        var values = [];
+        var key;
+    
+        for (key in object) {
+            values.push(object[key]);
+        }
+    
+        return config.Promise.all(values).then(function (results) {
+            return keys.reduce(function (fulfilledObject, key, index) {
+                fulfilledObject[key] = results[index];
+                return fulfilledObject;
+            }, {});
+        });
+    };
 }
